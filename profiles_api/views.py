afae13c3e6a5527893172b19e4bc3eac6d63f108
jjamescode <jalandajames@yahoo.com>
2020-08-01 10:53:57 -0500

    Added Profiles Viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,8 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -88,3 +91,11 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Partially updates an object"""
         return Response({'http_method': 'DESTROY'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Creates and updates profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
